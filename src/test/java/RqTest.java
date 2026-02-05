e94533022f0f7ce235845c8896563cc7069ceda8
lih8697-1 <lih8697@gmail.com>
2026-02-05 12:35:36 +0900

    t3 - gb, t4 - rg

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -1,30 +1,45 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RqTest {
+
     @Test
-    @DisplayName(value = "rq.getActionName() - 삭제")
+    @DisplayName(value = "명령: 삭제?id=1 일때, rq.getActionName() - 삭제")
     void t1() {
+
         Rq rq = new Rq("삭제?id=1");
         String actionName = rq.getActionName();
         assertThat(actionName).isEqualTo("삭제");
+
     }
 
     @Test
-    @DisplayName(value = "rq.getActionName() - 수정")
+    @DisplayName(value = "명령: 수정?id=1 일때, rq.getActionName() - 수정")
     void t2() {
+
         Rq rq = new Rq("수정?id=1");
         String actionName = rq.getActionName();
         assertThat(actionName).isEqualTo("수정");
+
     }
 
     @Test
-    @DisplayName(value = "명령: 수정?id=1일 때, rq.getParam(\"id\") - > 1")
+    @DisplayName(value = "명령: 목록?keyword=자바 일때, rq.getParam(\"kewyord\") -> \"자바\"")
     void t3() {
-        Rq rq = new Rq("수정?id=1");
-        int rst = rq.getParam("id");
-        assertThat(rst).isEqualTo(1);
+
+        Rq rq = new Rq("목록?keyword=자바");
+        String rst = rq.getParam("keyword");
+        assertThat(rst).isEqualTo("자바");
+    }
+
+    @Test
+    @DisplayName(value = "명령: 목록?keywordType=title 일때, rq.getParam(\"kewyordType\") -> \"title\"")
+    void t4() {
+
+        Rq rq = new Rq("목록?keywordType=title");
+        String rst = rq.getParam("keywordType");
+        assertThat(rst).isEqualTo("title");
     }
 }
