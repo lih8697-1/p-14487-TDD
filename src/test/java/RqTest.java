3782e3a5e446f3fb82474c4d8945a6fc4b5c6695
lih8697-1 <lih8697@gmail.com>
2026-02-05 14:34:37 +0900

    getParamAsInt 사용 부분 수정

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -49,7 +49,7 @@ public class RqTest {
     @DisplayName(value = "명령: 목록?page=1 일때, rq.getParamAsInt(\"page\") -> 1")
     void t6() {
         Rq rq = new Rq("목록?page=1");
-        int rst = rq.getParamAsInt("page");
+        int rst = rq.getParamAsInt("page", -1);
         assertThat(rst).isEqualTo(1);
     }
 
@@ -57,7 +57,7 @@ public class RqTest {
     @DisplayName(value = "명령: 목록?page=10 일때, rq.getParamAsInt(\"page\") -> 10")
     void t7() {
         Rq rq = new Rq("목록?page=10");
-        int rst = rq.getParamAsInt("page");
+        int rst = rq.getParamAsInt("page", -1);
         assertThat(rst).isEqualTo(10);
     }
 
